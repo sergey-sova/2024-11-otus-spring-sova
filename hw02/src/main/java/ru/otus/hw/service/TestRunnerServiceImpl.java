cecd25a6fcf41300262faba8240dfe52e8833aae
Sova, Sergey <asmi@inbox.ru>
2025-01-08 17:51:15 +0300

    Правки по ревью

--- hw02/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -1,10 +1,13 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
+import ru.otus.hw.exceptions.QuestionReadException;
 
 @Service
 @RequiredArgsConstructor
+@Log
 public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final TestService testService;
@@ -13,12 +16,18 @@ public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final ResultService resultService;
 
+    private final IOService ioService;
+
     @Override
     public void run() {
         var student = studentService.determineCurrentStudent();
-        var testResult = testService.executeTestFor(student);
-        if (testResult != null) {
+
+        try {
+            var testResult = testService.executeTestFor(student);
             resultService.showResult(testResult);
+        } catch (QuestionReadException e) {
+            ioService.printLine("Error reading the questions");
+            log.severe(e.getMessage());
         }
     }
 }
--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -2,18 +2,15 @@ package ru.otus.hw.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
-import ru.otus.hw.exceptions.QuestionReadException;
 
 @Service
 @RequiredArgsConstructor
-@Log
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
@@ -25,46 +22,50 @@ public class TestServiceImpl implements TestService {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
 
-        try {
-            var questions = questionDao.findAll();
-            var testResult = new TestResult(student);
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
 
-            for (var question: questions) {
-                test(question, testResult);
-            }
-            return testResult;
-        } catch (QuestionReadException e) {
-            ioService.printLine("Error reading the questions");
-            log.severe(e.getMessage());
-            return null;
+        for (var question: questions) {
+            boolean isAnswerValid = askQuestion(question);
+            testResult.applyAnswer(question, isAnswerValid);
         }
+        return testResult;
     }
 
-    private void test(Question question, TestResult testResult) {
-        ioService.printLine(question.text());
+    private boolean askQuestion(Question question) {
+        String questionText = convertToString(question);
+        ioService.printLine(questionText);
+
         var answers = question.answers();
-        int rightAnswerNumber = printAnswers(answers);
         int answerNumber = ioService.readIntForRangeWithPrompt(1, answers.size(),
                 "Please input the right answer number", "Answer number is out of range");
-        var isAnswerValid = answerNumber == rightAnswerNumber;
-        testResult.applyAnswer(question, isAnswerValid);
-        ioService.printLine("");
+        int rightAnswerNumber = getRightAnswerNumber(answers);
+        return answerNumber == rightAnswerNumber;
     }
 
-    /**
-     * Вывод ответов пользователю
-     *
-     * @param answers перечень ответов
-     * @return номер правильного ответа
-     */
-    private int printAnswers(List<Answer> answers) {
+    private String convertToString(Question question) {
+        StringBuilder sb = new StringBuilder("\n");
+        sb.append(question.text());
+        sb.append("\n");
+
+        var answers = question.answers();
+        for (int i = 0; i < answers.size(); i++) {
+            Answer answer = answers.get(i);
+            sb.append(i + 1);
+            sb.append(". ");
+            sb.append(answer.text());
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    private int getRightAnswerNumber(List<Answer> answers) {
         int rightAnswerNumber = 0;
         for (int i = 0; i < answers.size(); i++) {
             Answer answer = answers.get(i);
-            int number = i + 1;
-            ioService.printLine(number + ". " + answer.text());
             if (answer.isCorrect()) {
-                rightAnswerNumber = number;
+                rightAnswerNumber = i + 1;
             }
         }
         return rightAnswerNumber;
