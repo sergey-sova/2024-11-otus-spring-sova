c655393f707ceffff643ff4d857a044329c0df64
Sergey <asmi@inbox.ru>
2025-02-03 12:51:00 +0300

    hw04 (#4)
    * Решение
    
    * Правки по ревью
    
    * Правки по ревью

---
+++ hw04/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -0,0 +1,32 @@
+package ru.otus.hw.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.otus.hw.config.TestFileNameProvider;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = CsvQuestionDao.class)
+class CsvQuestionDaoTest {
+
+    @MockBean
+    private TestFileNameProvider fileNameProvider;
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    @BeforeEach
+    void setUp() {
+        when(fileNameProvider.getTestFileName()).thenReturn("questions.csv");
+    }
+
+    @Test
+    void findAllTest() {
+        assertEquals(5, questionDao.findAll().size());
+    }
+
+}
---
+++ hw04/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,32 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Student;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(classes = TestServiceImpl.class)
+class TestServiceImplTest {
+
+    @MockBean
+    private LocalizedIOService ioService;
+
+    @MockBean
+    private QuestionDao questionDao;
+
+    @Autowired
+    private TestService testService;
+
+    @Test
+    void executeTest() {
+        Student student = new Student("Aygul", "Yusupova");
+        testService.executeTestFor(student);
+        verify(ioService).printLine(anyString());
+        verify(questionDao).findAll();
+    }
+}
